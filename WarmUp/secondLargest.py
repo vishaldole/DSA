65cc330771aea7603f3b038101bff7f6a7a97c71
vishaldole <vishaldole.1998@gmail.com>
2026-09-19 09:00:34 +0530

    python

--- WarmUp/secondLargest.py
+++ WarmUp/secondLargest.py
@@ -11,4 +11,28 @@ def secondLargest(li):
 
     return l2
 
-print(f'second largest element is {secondLargest([-4, -9, -10, -2, -8, -7, -1])}')
+print(f'second largest element is {secondLargest([ 4, 1 ])}')
+
+# considering corner cases
+## 1. what if array is null
+## 2. what if array has only one element
+
+### In these cases, return null. Only give solutions for arr size equal or greater than 2
+
+def secondLargest(li):
+
+    if len(li) < 2:
+        return None
+    l1 = float('-inf')
+    l2 = float('-inf')
+
+    for i in range(len(li)):
+        if li[i] > l1:
+            l2 = l1
+            l1 = li[i]
+        elif li[i] > l2:
+            l2 = li[i]
+
+    return l2
+
+print(f'second largest element is {secondLargest([6, 6, 5, 4])}')
