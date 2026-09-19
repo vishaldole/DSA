a51c9487b927c91f925d4aafb31b8d65c4211f76
vishaldole <vishaldole.1998@gmail.com>
2026-09-19 09:16:03 +0530

    corner cases for 2nd largest element

--- WarmUp/secondLargest.py
+++ WarmUp/secondLargest.py
@@ -17,7 +17,7 @@ print(f'second largest element is {secondLargest([ 4, 1 ])}')
 ## 1. what if array is null
 ## 2. what if array has only one element
 
-### In these cases, return null. Only give solutions for arr size equal or greater than 2
+### In these cases, return None. Only give solutions for arr size equal or greater than 2
 
 def secondLargest(li):
 
@@ -35,4 +35,53 @@ def secondLargest(li):
 
     return l2
 
-print(f'second largest element is {secondLargest([6, 6, 5, 4])}')
+print(f'second largest element is {secondLargest([6])}')
+
+## 3. Does it handles negative elements 
+
+### yes, it handles
+
+## 4. Does it handles duplicate elements, i.e., for [10, 20, 20] -> it still returns 20, but ideally it should return 10
+
+### This can be handled by giving condition that second largest can't be equal to first largest
+
+def secondLargest(li):
+
+    if len(li) < 2:
+        return None
+    l1 = float('-inf')
+    l2 = float('-inf')
+
+    for i in range(len(li)):
+        if li[i] > l1:
+            l2 = l1
+            l1 = li[i]
+        elif li[i] > l2 and li[i] != l1:
+            l2 = li[i]
+    
+    return l2
+
+print(f'second largest element is {secondLargest([6, 6, 1, 6])}')
+
+## 5. what if array has only duplicates [6,6,6,6,6] -> returns -inf, it should be handled
+### give condition that if l2 is -inf, return None
+
+def secondLargest(li):
+
+    if len(li) < 2:
+        return None
+    l1 = float('-inf')
+    l2 = float('-inf')
+
+    for i in range(len(li)):
+        if li[i] > l1:
+            l2 = l1
+            l1 = li[i]
+        elif li[i] > l2 and li[i] != l1:
+            l2 = li[i]
+
+    if l2 == float('-inf'):
+        return None
+    return l2
+
+print(f'second largest element is {secondLargest([6, 6, 6, 6])}')
---
+++ WarmUp/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+def secondLargest(li):
+#     l1 = float('-inf')
+#     l2 = float('-inf')
+
+#     for i in range(len(li)):
+#         if li[i] > l1:
+#             l2 = l1
+#             l1 = li[i]
+#         elif li[i] > l2:
+#             l2 = li[i]
+
+#     return l2
+
+# print(f'second largest element is {secondLargest([ 4, 1 ])}')
